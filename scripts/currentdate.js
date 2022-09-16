487c2c45c5c00e46df744d2262c315ba8418db69
GloRosado07/wdd230 <address>
2022-09-15 21:31:55 -0600

    new changes

---
+++ scripts/currentdate.js
@@ -0,0 +1,44 @@
+const lastmod = document.querySelector('#lastmod');
+lastmod.textContent = `Last Updated: ${document.lastModified}`;
+
+
+//tolocaleDateString
+//const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+//day names array
+//const daynames = [
+//    "Sunday",
+//    "Monday",
+//    "Tuesday",
+//    "Wednesday",
+//    "Thursday",
+//    "Friday",
+//    "Saturday",
+//];
+
+//long month names array
+//const months = [
+//    "January",
+//    "February",
+//    "March",
+//    "April",
+//    "May",
+//    "June",
+//    "July",
+//    "August",
+//    "September",
+//    "October",
+//    "November",
+//    "December"
+//];
+
+//const todaysdate = new Date ();
+//const dayName = daynames[todaysdate.getDate()];
+//const monthName = months[todaysdate.getMonth()];
+//const year = todaysdate.getFullYear
+//const currentdate = dayName + " , " + todaysdate.getDate() + " " + monthName + " , " + todaysdate.getFullYear();
+//document.getElementById('currentdate').textContent = currentdate;
+
+
+//document.getElementByID('currentdate').textContent = new Date().tolocalDateString('en-US', options);
+
+
