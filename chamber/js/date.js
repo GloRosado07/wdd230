be71ca8ea1031f820806cedd595e37ece671faee
GloRosado07/wdd230 <address>
2022-10-14 09:19:45 -0600

    updates

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -3,3 +3,18 @@ const year = d.getFullYear();
 
 document.querySelector("#currentyear").textContent = year;
 document.querySelector("#pagelastupdated").innerHTML = `Last update: ${document.lastModified}`;
+
+// element to output to
+const datefieldUK = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+if(d.getDay() === 2 || d.getDay() === 1) {
+	const banner = document.querySelector("#banner");
+	banner.style.display = "block";
+}
