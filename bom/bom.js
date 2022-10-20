809b5a91b707eac65a8140a9d1a4e3f6f9d828dc
GloRosado07/wdd230 <address>
2022-10-19 20:16:22 -0600

    javascript update

--- bom/bom.js
+++ bom/bom.js
@@ -2,19 +2,26 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const output = document.querySelector('.list');
+function empty(input){
+    if(input.value.length == 0) {
+        return false;
+    }
+    return true;
+}
 
 //Add an event listener to the button.
 button.addEventListener('click', () => {
-    let li = document.createElement('li');
-    let deletebutton = document.createElement('button');
-    li.textContent = input.value;
-    deletebutton.textContent = 'X';
-    li.append(deletebutton);
-    output.append(li);
-    deletebutton.addEventListener('click', function() {
-        output.removeChild(li);
-        input.focus;
-    })
+    if(empty(input)){
+        let li = document.createElement('li');
+        let deletebutton = document.createElement('button');
+        li.textContent = input.value;
+        deletebutton.textContent = 'X';
+        li.append(deletebutton);
+        output.append(li);
+        deletebutton.addEventListener('click', function() {
+            output.removeChild(li);
+            input.focus;
+        })}
     input.value = '';
     input.focus;
 })
