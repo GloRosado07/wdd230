90ed38906e6102c437072d8ee79ac4ee9a2e6a38
GloRosado07/wdd230 <address>
2022-11-07 20:21:01 -0600

    updates

--- chamber/js/pagevisited.js
+++ chamber/js/pagevisited.js
@@ -2,11 +2,11 @@ const lastvisit_text = window.localStorage.getItem('lastvisit');
 if(lastvisit_text === undefined) {
     lastvisit_text = new Date(Data.now());
 }
-const lastvisit = Date.parse(lastvisit_text);
+const last_visit = Date.parse(lastvisit_text);
 const FACTOR = 1000 * 60 * 60 * 24;
 
 
-let daysbetween = Date.now() - lastvisit;
+let daysbetween = Date.now() - last_visit;
 let numberOfDays = daysbetween / FACTOR;
 
 window.localStorage.setItem('lastvisit', new Date(Date.now()));
