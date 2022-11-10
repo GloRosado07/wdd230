3082550a041411cf1903a0ed0ed4acf65ea94c7d
GloRosado07/wdd230 <address>
2022-11-09 21:41:13 -0600

    updates

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -1,22 +1,33 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-
+const cards = document.querySelector('.cards');
 
 fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        const prophets = jsonObject['prophets'];
-        // console.table(jsonObject);
-        prophets.forEach(displayProphets);
-    });
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const prophets = jsonObject['prophets'];
+    // console.table(jsonObject);
+    prophets.forEach(displayProphets);
+});
 
 function displayProphets(prophet) {
-  let card = document.createElement('section');
-
-  //h2 name
+    let card = document.createElement('section');
+    let suffix = ""
+    let sequence = prophet.order.valueOf()
+    if (sequence == 1) {
+      suffix = "st";
+    } else if (sequence == 2) {
+      suffix = "nd";
+    } else if (sequence == 3) {
+      suffix = "rd"
+    } else {
+      suffix = "th"
+    }
+  
+    //h2 name
   let h2 = document.createElement('h2');
-  h2.textContent = `${prophet.name} ${prophet.lastname}`;
+  h2.innerHTML = `<h2>${prophet.name} ${prophet.lastname}<br> ${prophet.order}${suffix} President</h2>`
   card.appendChild(h2);
 
   //p birth date
@@ -34,10 +45,6 @@ function displayProphets(prophet) {
   image.src = prophet.imageurl;
   image.setAttribute('alt', prophet.order);
   card.appendChild(image);
-
-
-
-
+  
   document.querySelector('div.cards').appendChild(card);
-
 }
